c261f4ea90b8dce981b8392394dc8ed800124682
Akshit <akshit@Akshits-MBP.Dlink>
2020-02-19 09:11:24 +0530

    Pointers

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 )
 
+/*
+  Comment out the methods on which type you want to test
+*/
 func main() {
-	simplePrint()
-	primitiveDataTypes()
+	// simplePrint()
+	// primitiveDataTypes()
+	pointers()
 }
 
 func simplePrint() {
@@ -32,4 +36,23 @@ func primitiveDataTypes() {
 
 	r, im := real(c), imag(c)
 	fmt.Println(r, im)
+}
+
+func pointers() {
+	var firstName *string = new(string)
+
+	*firstName = "Akshit"
+
+	fmt.Println(firstName)
+	fmt.Println(*firstName)
+
+	lastName := "Goel"
+
+	fmt.Println(lastName)
+
+	ptr := &lastName
+	fmt.Println(ptr, *ptr)
+
+	lastName = "Aggarwal"
+	fmt.Println(ptr, *ptr)
 }
