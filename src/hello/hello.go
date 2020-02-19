9debc40c579e8359fd22728dcfc98f8647551557
Akshit <akshit@Akshits-MBP.Dlink>
2020-02-19 09:00:04 +0530

    Primitive data types

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
-  simplePrint()
+func main() {
+	simplePrint()
+	primitiveDataTypes()
 }
 
-func simplePrint(){
-  fmt.Printf("hello, world\n")
-  fmt.Printf("how are you\n")
+func simplePrint() {
+	fmt.Printf("hello, world\n")
+}
+
+func primitiveDataTypes() {
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Akshit"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
