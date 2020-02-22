6071da1613c4762cc8118c962d99617b5c44f600
Akshit <akshit@Akshits-MBP.Dlink>
2020-02-22 22:08:43 +0530

    Map

--- src/primitiveCourse/collections.go
+++ src/primitiveCourse/collections.go
@@ -4,7 +4,24 @@ import "fmt"
 
 func main() {
 	// array()
-	slice()
+	// slice()
+	mapExample()
+}
+
+func mapExample() {
+
+	m := map[string]int{"foo": 42}
+
+	fmt.Println(m)
+	fmt.Println(m["foo"])
+
+	m["foo"] = 27
+
+	fmt.Println(m)
+
+	delete(m, "foo")
+
+	fmt.Println(m)
 }
 
 func slice() {
